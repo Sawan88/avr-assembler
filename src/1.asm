efbf3d2bb086f930fcf937f7e8df411c338e2dbf
Sergey Ivanov <ivanovknitting@gmail.com>
2023-05-26 12:31:25 +0300

    Removing excess

--- src/1.asm
+++ src/1.asm
@@ -1,14 +1,13 @@
 .DEVICE ATtiny2313A
 
-; Configure controler
+; Configure PORTB and PORTD for output
 SER r16
 CLR r17
 OUT 0x11, r16
 OUT 0x17, r16
 
 loop:
-
-; turn on
+; blink diodes 0,2,4,6
 LDI r30, 0xfa
 OUT 0x12, r30
 LDI r31, 0xf5
@@ -31,32 +30,28 @@ BRNE Delay2
 DEC r20
 BRNE Delay1
     
-;turn off
+; blink diodes 1,3,5,7
 LDI r30, 0xfd
 OUT 0x12, r30
 LDI r31, 0xea
 OUT 0x18, r31
 
 Delay_2sec:
-    LDI r23, 8
+    LDI r20, 8
 Delay4:
-    LDI r24, 125
+    LDI r21, 125
 Delay5:
-    LDI r25, 250
+    LDI r22, 250
 Delay6:
-DEC r25
+DEC r22
 NOP
 BRNE Delay6
 
-DEC r24
+DEC r21
 BRNE Delay5
 
-DEC r23
+DEC r20
 BRNE Delay4
-
-;LDI r16, 0xfc
-;OUT 0x12, r16
 
 NOP
 RJMP loop
-;avrdude -p t2313 -c usbasp -U flash:w:1.hex:i
